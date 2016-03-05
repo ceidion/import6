d92fe14dda6169a7f6e5c82b89b7c4ded7287f04
maestropanel2 <ping@maestropanel.com>
2016-03-05 18:51:19 +0200

    init

---
+++ src/import6/import6/Models.cs
@@ -0,0 +1,16 @@
+﻿namespace import6
+{
+    public class Domain
+    {
+
+        public string Name { get; set; }
+        public string MetaName { get; set; }
+        public string State { get; set; }
+        public bool HasSSL { get; set; }
+
+        //ErrorDocs
+        //Mimetypes
+        //Bindings
+        //CustomHeaders        
+    }
+}
---
+++ src/import6/import6/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace import6
+{
+    using System.Collections.Generic;
+    using System.Management;    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        private static List<string> GetWebSiteList()
+        {
+            var _tmp = new List<string>();
+
+            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\MicrosoftIISv2", "SELECT * FROM IIsWebServerSetting"))
+            {
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    if (queryObj["ServerComment"] != null)
+                        _tmp.Add(queryObj["ServerComment"].ToString());
+                }                
+            }
+
+            return _tmp;
+        }        
+    }    
+}
