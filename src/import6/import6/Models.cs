f4551ef5115d1ae4f1a1906e134ac60f6da1dddc
maestropanel2 <ping@maestropanel.com>
2016-03-11 15:09:49 +0200

    Designed Domain Model

--- src/import6/import6/Models.cs
+++ src/import6/import6/Models.cs
@@ -8,9 +8,26 @@
         public string State { get; set; }
         public bool HasSSL { get; set; }
 
-        //ErrorDocs
-        //Mimetypes
-        //Bindings
-        //CustomHeaders        
+        public CustomHeader[] Headers { get; set; }
+        public MimeType[] MimeTypes { get; set; }
+        public CustomError[] CustomErrors { get; set; }                        
+    }
+
+    public class CustomHeader
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+
+    public class MimeType
+    {
+        public string Extension { get; set; }
+        public string MType { get; set; }
+    }
+
+    public class CustomError
+    {
+        public string MessageType { get; set; }
+        public string MessageValue { get; set; }
     }
 }
